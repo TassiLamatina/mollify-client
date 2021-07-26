6613f88f2d14881657359979165ebdb2fe537de0
Tlamatina <Tassi.lamatina@gmail.com>
2021-07-26 10:25:45 -0700

    NewTask added

---
+++ components/NewTask.jsx
@@ -0,0 +1,52 @@
+import { Form, Button } from "react-bootstrap"
+
+export default function NewTask(props) {
+    return(
+        <>  
+            <h4>Create Task</h4>
+
+            <Form.Group controlId="exampleForm.ControlInput1" className="mb-2">
+                <Form.Label>Task Title</Form.Label>
+                <Form.Control type="taskTiltle" value={props.title} onChange={(e) => props.setTitle(e.target.value)} placeholder="Task Title" />
+            </Form.Group>
+
+            <Form.Group controlId="exampleForm.ControlTextarea1" className="mb-2">
+                <Form.Label> Description</Form.Label>
+                <Form.Control as="textarea" rows={3} value={props.description} onChange={(e) => props.setDescription(e.target.value)} placeholder="Description"/>
+            </Form.Group>
+
+             <Form.Group controlId="exampleForm.ControlTextarea1" className="mb-3">
+                <Form.Label>Notes</Form.Label>
+                <Form.Control as="textarea" rows={3} value={props.notes} onChange={(e) => props.setNotes(e.target.value)} placeholder="Notes"/>
+            </Form.Group>
+
+            <div className="add-date">
+                <label className="mb-2 px-1"id="calendar" htmlFor="start">Added on:</label>
+                <input className="mb-2" type="date" id="start" name="trip-start"
+                    value={props.dateApplied} onChange={(e) => props.setDateApplied(e.target.value)} 
+                    min="2018-01-01" max="2035-12-31"/>  
+            </div>        
+            
+            <Form.Group controlId="exampleForm.ControlSelect1" className="mb-3">
+                <Form.Label>Priority:</Form.Label>
+                <Form.Control as="select" value={props.priority} onChange={(e) => props.setPriority(e.target.value)}>
+                    <option>High</option>
+                    <option>Medium</option>
+                    <option>Low</option>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="exampleForm.ControlSelect1" className="mb-4">
+                <Form.Label>Status:</Form.Label>
+                <Form.Control as="select" value={props.status} onChange={(e) => props.setStatus(e.target.value)}>
+                    <option>Incomplete</option>
+                    <option>Completed</option>
+                    
+                </Form.Control>
+            </Form.Group>
+            <div className="create-btn">
+                <Button id="createCard" onClick={() => props.handleTaskCreate()}>Submit</Button>
+            </div>
+        </>
+    )
+}
---
+++ components/TaskList.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+// styling for priority coloring
+const PrioritySpan = styled.div`
+background-color: ${props => {
+  if(props.priority === "High"){
+    return "#F97272"
+  }
+  else if(props.priority === "Medium"){
+     return "#F9F372"
+  }
+     else if(props.priority === "Low"){
+     return "#9DF972"
+     }
+     else{
+       return "white"
+     }
+     }};
+     box-shadow: 0px 12px 0px #FFFFFF;
+     border-radius: 200.5381px;
+`;
+
+const TaskList = (props) => {
+    // render each task from TaskList.js
+    const renderedTasks = props.taskData.map(task => {
+        return(
+            <li key={`${task._id}`}>
+                <button id="tasktiles" onClick={() => props.handleTaskCardClick(task)}> 
+                <PrioritySpan priority={`${task.priority}`} id="priority">{task.priority}<br></br></PrioritySpan>
+                <span id="tileTitle">{task.title} <br></br></span>
+                <span id="tileAdded">{task.dateAdded}</span>
+                </button>
+                <br></br>
+                <br></br>
+            </li>
+        )
+    })
+
+    return(
+        <div>
+            
+            <ul className='list'>
+                {renderedTasks}
+            </ul>
+        </div>
+    )
+}
+export default TaskList
