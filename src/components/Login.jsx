e3e229338bdb777f0664035b36d675e1b88666c5
Tlamatina <Tassi.lamatina@gmail.com>
2021-07-30 21:50:09 -0700

    better style

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,8 +2,54 @@ import LandingPage from './LandingPage.jsx'
 import axios from 'axios'
 import jwt from 'jsonwebtoken'
 import { Redirect, Link } from 'react-router-dom'
+import styled from "styled-components";
 
 import { useState } from 'react'
+
+const TitleComponent = styled.h1`
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Code', monospace;
+  width: 100%;
+  height: 100%;
+  font-size: 16px;
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  position: relative;
+  display: inline-block;
+  font-size: 3rem;
+  background: linear-gradient(to bottom, #000, #000 60%, #fff 60%, #fff 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  background-repeat: no-repeat;
+  transition: background 0.2s ease-out;
+  white-space: nowrap;
+  position: relative;
+
+span:before {
+  content: "";
+  position: absolute;
+  left: 0;
+  right: 0;
+  height: 15px;
+  background: #000;
+  bottom: 9px;
+  transition: all 0.2s ease-out;
+}
+
+p:hover {
+  background-position: 0 11px;
+}
+
+span:hover:before {
+  transform: translateY(10px)
+}
+`;
 
 export default function Login(props) {
 
@@ -52,29 +98,32 @@ export default function Login(props) {
       // console.log(` hit me with your best shot! ${props}`)
 
     return(
-        <div className="list-group-item list-group-item-action flex-column align-items-start" id="sign-in-container"> 
-            <div className="d-flex w-100 justify-content-between">
-                <div className="container">
-                    <form onSubmit={handleSubmit} id="sign-in">
-                        <div className="horizontal-center vertical-center">
-                            <label className="form-label">SIGN IN:</label>
-                            <input onChange={e => setEmail(e.target.value) } className="form-control" value={email} type="text" name="username" placeholder="email"/>
-                        </div>
-                        
-                        <div className="">
-                            <input id="passwordPad" onChange={e => setPassword(e.target.value) } className="form-control" value={password} type="password" name="password" placeholder="Password"/>
-                            <input className="sign-in-submit" type="submit"></input>                 
-                        </div>
-                    </form>
-                    <p className="">{message}</p>
-                    <div className="account text-center">
-                        <p>Don't have an account?</p>
-                        <Link to="/register">
-                            <button type="button" id="account-register">Create Account</button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <div> 
+        < TitleComponent classname="logo-1"><p>Mollify</p></TitleComponent>
+          <div className="list-group-item list-group-item-action flex-column align-items-start" id="sign-in-container"> 
+              <div className="d-flex w-100 justify-content-between">
+                  <div className="container">
+                      <form onSubmit={handleSubmit} id="sign-in">
+                          <div className="horizontal-center vertical-center">
+                              <label className="form-label">SIGN IN:</label>
+                              <input onChange={e => setEmail(e.target.value) } className="form-control" value={email} type="text" name="username" placeholder="email"/>
+                          </div>
+                          
+                          <div className="">
+                              <input id="passwordPad" onChange={e => setPassword(e.target.value) } className="form-control" value={password} type="password" name="password" placeholder="Password"/>
+                              <input className="sign-in-submit" type="submit"></input>                 
+                          </div>
+                      </form>
+                      <p className="">{message}</p>
+                      <div className="account text-center">
+                          <p>Don't have an account?</p>
+                          <Link to="/register">
+                              <button type="button" id="account-register">Create Account</button>
+                          </Link>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
     )
 }
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,5 +1,51 @@
 import { Link } from 'react-router-dom'
 import{ Navbar, Col }from 'react-bootstrap'
+import styled from "styled-components";
+
+const TitleComponent = styled.h1`
+padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Code', monospace;
+  width: 100%;
+  height: 100%;
+  font-size: 16px;
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  position: relative;
+  display: inline-block;
+  font-size: 3rem;
+  background: linear-gradient(to bottom, #000, #000 60%, #fff 60%, #fff 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  background-repeat: no-repeat;
+  transition: background 0.2s ease-out;
+  white-space: nowrap;
+  position: relative;
+
+span:before {
+  content: "";
+  position: absolute;
+  left: 0;
+  right: 0;
+  height: 15px;
+  background: #000;
+  bottom: 9px;
+  transition: all 0.2s ease-out;
+}
+
+p:hover {
+  background-position: 0 11px;
+}
+
+span:hover:before {
+  transform: translateY(10px)
+}
+`;
 
 export default function NavigationBar(props) {
     // if the user is logged in
@@ -7,7 +53,7 @@ export default function NavigationBar(props) {
         <>
         <Navbar className="navbar navbar-expand-lg ">
             <Col xs={6} md={10}>
-                <Navbar.Brand className="container-fluid" id="mollify">Mollify</Navbar.Brand>
+                <TitleComponent className="container-fluid" id="mollify"><p>Mollify</p></TitleComponent>
             </Col>
             <Col xs={4} md={2} className="user-col">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
